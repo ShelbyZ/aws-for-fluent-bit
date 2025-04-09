275946c5e19399f2856babbd7fa005866b7d3500
Shelby Hagman <shelbyzh@amazon.com>
2025-04-09 21:18:22 +0000

    Skip image check for testing images before release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -604,16 +604,17 @@ check_image_version() {
 	export DOCKER_CLI_EXPERIMENTAL=enabled
 	EXIT_CODE=0
 
-	docker_hub_login
+	# docker_hub_login
 	
-	# check if we can get the image information in dockerhub; if yes, the exit status should be 0
-	docker manifest inspect public.ecr.aws/aws-observability/aws-for-fluent-bit:${1} > /dev/null || EXIT_CODE=$?
-	if [ "${EXIT_CODE}" = "0" ]; then
-		echo "Accidental release: current image version from github source file match a previous version from dockerhub."
-		exit 1
-	fi
+	# # check if we can get the image information in dockerhub; if yes, the exit status should be 0
+	# docker manifest inspect public.ecr.aws/aws-observability/aws-for-fluent-bit:${1} > /dev/null || EXIT_CODE=$?
+	# if [ "${EXIT_CODE}" = "0" ]; then
+	# 	echo "Accidental release: current image version from github source file match a previous version from dockerhub."
+	# 	exit 1
+	# fi
 
-	echo "Approved release: release the image with a new version."
+	# echo "Approved release: release the image with a new version."
+	echo "Skipping image version check for testing"
 }
 
 verify_ecr_image_scan() {
